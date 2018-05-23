a1231d693d487787a7c111b812bdd46ace9cfa6c
Raphael Croce <rcroce7@gmail.com>
2018-05-22 21:40:27 -0500

    Create Entry class component

---
+++ src/components/entry.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//Create stateful Entry component
+class Entry extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
